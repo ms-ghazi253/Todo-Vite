df82acc73917024152b509ede8deebd9a1430d8e
ms.ghazi253 <ms.ghazi253@gmail.com>
2022-07-05 14:21:55 +0430

    config Axios Api

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 
-axios.defaults.baseURL = 'https://vue-course-7f9dd-default-rtdb.europe-west1.firebasedatabase.app'
+
 createApp(App).mount('#app')
